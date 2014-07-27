07c69cc1f1ff437866839bac846d6bbc9877c6b7
Phongsak Ritpitakphong <poorprogrammer@phongsaks-mbp.lan>
2014-07-27 23:23:50 +0700

    Spike to share controller between Directive by using individual controller instance

--- app/directives/CommunicationBetweenDirective.js
+++ app/directives/CommunicationBetweenDirective.js
@@ -4,50 +4,42 @@ function AnimalController(){
 
   this.info = "";
   this.count = 1;
+  
   this.walk4Legs = function() {
-    this.info = "Walking by 4 legs"
+    this.count += 1;
+    this.info = "Walking by 4 legs";    
+    console.log(this.info + '=' + this.count);
+    
   };
 
 }
 
-
-app.directive("animal",function(){
-  return {
-    controller: AnimalController,
-    controllerAs : "animalController"   
-  };
-});
+app.controller("AnimalController",[AnimalController]);
+// app.directive("animal",function(){
+//   return {
+//     controller: AnimalController,
+//     controllerAs : "animalController"   
+//   };
+// });
 
 app.directive("dog", function () {
   return {
     restrict: "E",
-    require:'^animal',
-    template : '<input type="button" ng-click="walk()" name="buttonDog" value="DogDirective"/>',
-    scope : {},
-    link : function(scope, element, attrs, animalController){
-      scope.walk = function(){
-        animalController.count += 1;
-        animalController.walk4Legs();
-      }
-    }
+    controller : 'AnimalController',
+    controllerAs : 'animalController',
+    template : '{{animalController.info}}<input type="button" ng-click="animalController.walk4Legs()" name="buttonDog" value="DogDirective"/>',
+    scope : {}
   };
+  
 });
 
 app.directive("cat", function() {
   return {
     restrict: "E",
-    require:'^animal',
-    template : '<input type="button" ng-click="localFunctionForCat()" name="buttonCat" value="CatDirective"/>',
-    scope : {},
-    link : function(scope, element, attrs, animalController){
-      scope.catchMouse = function(){
-        animalController.walk4Legs();
-      };
-
-      scope.localFunctionForCat = function(){
-        animalController.count += 1;
-        animalController.info = "I will do whatever it take to study this directive";
-      }
-    }
+    controller : 'AnimalController',
+    controllerAs : 'animalController',
+    template : '{{animalController.info}}<input type="button" ng-click="animalController.walk4Legs()" name="buttonCat" value="CatDirective"/>',
+    scope : {}
+    
   };
 });
